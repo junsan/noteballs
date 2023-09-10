a2c074c093379cc0c523a1d63ecfb5b1d9a41efa
Jun <xjunsantos@gmail.com>
2023-09-11 00:02:06 +0800

    Navbar design

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <RouterLink to="/">Notes</RouterLink> | 
-    <RouterLink to="/stats">Stats</RouterLink>
+    <Navbar></Navbar>
+    <!-- <RouterLink class="button" to="/">Notes</RouterLink> | 
+    <RouterLink class="button" to="/stats">Stats</RouterLink> -->
 
     <RouterView />
   </div>
 </template>
+
+<script setup>
+import Navbar from './components/layout/Navbar.vue';
+</script>
+
+<style>
+  @import 'bulma/css/bulma.min.css'
+</style>
 
 
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+        <div class="container is-max-desktop">
+            <div class="navbar-brand">
+                <div class="navbar-item is-size-4">Noteballs</div>
+
+                <a role="button" class="navbar-burger" :class="{ 'is-active': showDropDown }" 
+                @click.prevent="showDropDown = !showDropDown" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showDropDown }">
+                <div class="navbar-start">
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <RouterLink class="navbar-item" active-class="is-active" to="/">
+                            Notes
+                        </RouterLink>
+                        <RouterLink class="navbar-item" active-class="is-active" to="/stats">
+                            Stats
+                        </RouterLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showDropDown = ref(false)
+
+</script>
